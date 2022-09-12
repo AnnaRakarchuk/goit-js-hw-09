9832fb018a4eb2e77e3e92ed58a8a58f4389bae9
Anna Rakarchuk <anytarakarchuk@gmail.com>
2022-09-12 18:47:42 +0300

    Add code task 01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+let timer = null;
+const TIME_DELAY = 1000;
+
+startBtn.addEventListener('click', startChangeColor);
+stopBtn.addEventListener('click', stopChangeColor);
+
+function startChangeColor(){
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    timer = setInterval(colorChangeHandler, TIME_DELAY);
+    colorChangeHandler();
+    console.log('start');
+}
+
+function stopChangeColor(){
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+    clearInterval(timer);
+    console.log('stop');
+}
+
+function colorChangeHandler(){
+    bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
 
